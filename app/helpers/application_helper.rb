8b8ee1f2e7137faf320c47487be2f879eb842ad9
Jamal <jamensky1991@gmail.com>
2021-04-14 01:15:53 +0100

    commit tree add155dbcf09f2e1f36739b6dc57140446aafe20

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def status(value)
+      if value == false 
+        p "This student is currently inactive."
+      else 
+        p "This student is currently active."
+      end 
+    end
+     
 end
